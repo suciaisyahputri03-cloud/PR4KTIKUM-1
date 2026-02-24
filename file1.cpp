85c2a0c24cb54e17e43e6246213715ead7e2f836
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-02-24 20:06:48 +0700

    menambahkan fungsi utama

--- file1.cpp
+++ file1.cpp
@@ -8,3 +8,21 @@ int main(){
     cout << "ini string" << endl;
     cout << 123 << endl;
 }
+int main() { //mulai
+    int p, l, luas; // deklarasi
+
+    //langkah 2 
+    cout << "masukkan panjang = ";
+    cin >> p;
+
+    //langkah 3
+    cout << "masukkan lebar = ";
+    cin >> l;
+
+    //langkah 4
+    luas = p * l;
+
+    //langkah 5
+    cout << "hasilnya = " << luas << endl;
+    cout << "terima kasih ";
+} //selesai
